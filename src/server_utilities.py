51d913602d1d55946fe8b5ece58ab4d7f1bf58a1
hbrooks <seanhunterbrooks@gmail.com>
2020-03-29 17:02:22 -0400

    support creation of internal/ routes;

--- src/server_utilities.py
+++ src/server_utilities.py
@@ -7,13 +7,13 @@ from flask_cors import CORS
 
 from flask import current_app
 from flask import g
+from flask import Blueprint
 from flask_sqlalchemy import SQLAlchemy
 from flask_sqlalchemy_session import flask_scoped_session
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
 from .exceptions import HouseKeepingBaseException
-
 
 LOG = logging.getLogger(__name__)
 database_connection_manager = None
@@ -27,14 +27,24 @@ def create_dict_from_env_vars(env_vars):
 
 class HouseKeepingApiServer:
 
-    HEALTH_CHECK_URI = '/healthCheck'
-
-    def __init__(self, models, blueprints, build_app_context_function=None, before_request_function=None, health_check_function=None):
+    def __init__(
+            self,
+            models,
+            blueprints,
+            build_app_context_function=None,
+            before_request_function=None,
+            health_check_function=None,
+            include_db_clean_endpoint=True):
         self.models = models
         self.blueprints = blueprints
         self.build_app_context_function = build_app_context_function
         self.before_request_function = before_request_function
-        self.health_check_function = health_check_function
+        self.health_check_function = health_check_function if health_check_function != None else self._get_default_heathCheck_function()
+        self.include_db_clean_endpoint = include_db_clean_endpoint
+
+    def _get_default_heathCheck_function(self):
+        def _DEFAULT_HEALTH_CHECK_FUNCITON(_):
+            return '', 200
 
     def build_flask_app(self):
 
@@ -54,16 +64,21 @@ class HouseKeepingApiServer:
             def before_request_function_():
                 self.before_request_function(self)
 
-        if self.health_check_function:
-            @app.route(self.HEALTH_CHECK_URI, methods=('GET',))
-            def health_check_function_():
-                return self.health_check_function(self)
+        internal_blueprint = self._build_internal_routes(
+            self.health_check_function,
+            self.include_db_clean_endpoint
+        )
+        self.blueprints.add(internal_blueprint)
 
-        else:
-            @app.route(self.HEALTH_CHECK_URI, methods=('GET',))
-            def DEAFULT_HEALTH_CHECK_FUNCITON():
-                return '', 200
+        # if self.health_check_function:
+        #     @app.route(self.HEALTH_CHECK_URI, methods=('GET',))
+        #     def health_check_function_():
+        #         return self.health_check_function(self)
 
+        # else:
+        #     @app.route(self.HEALTH_CHECK_URI, methods=('GET',))
+        #     def DEAFULT_HEALTH_CHECK_FUNCITON():
+        #         return '', 200
 
         @app.errorhandler(HouseKeepingBaseException)
         def handle_housekeeping_exceptions(e):
@@ -84,11 +99,43 @@ class HouseKeepingApiServer:
             LOG.error('------------------')
             return response_body, 500,  {'Content-Type': 'application/json'}
 
-        for blueprint_ in self.blueprints:
-            app.register_blueprint(blueprint_)
+        for current_blueprint in self.blueprints:
+            app.register_blueprint(current_blueprint)
 
         return app
 
+    def _build_internal_routes(
+        self,
+            health_check_function,
+            include_db_clean_endpoint
+        ):
+        
+        internal_routes_blueprint = Blueprint(
+            'internal',
+            __name__,
+            url_prefix='/internal')
+
+        if include_db_clean_endpoint:
+            @internal_routes_blueprint.route("/db/clean", methods=('POST',))
+            def clean_database():
+                TABLES_TO_TRUNCATE = { # TODO: Customize this for each service.
+                    'users',
+                    'events',
+                    'user_events',
+                    'user_activities'
+                }
+                db = g.session()
+                for table in TABLES_TO_TRUNCATE:
+                    db.execute('truncate {};'.format(table))
+                db.commit()
+                
+                return '', 200
+
+        @internal_routes_blueprint.route('/healthCheck', methods=('GET',))
+        def health_check_function_():
+            return health_check_function(self)
+
+        return internal_routes_blueprint
 
     
     def _configure_logging(self):
